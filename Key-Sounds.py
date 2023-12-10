bec0df292628eb7a252623412701851420044216
k754a <114301997+k754a@users.noreply.github.com>
2023-12-10 08:15:05 -0700

    Update Key-Sounds.py

--- Key-Sounds.py
+++ Key-Sounds.py
@@ -5,17 +5,17 @@ import keyboard
 import threading
 
 
-# Initialize pygame mixer
+
 pygame.mixer.init()
 
-# Get the current script directory
+
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
-# Load a sound file from the same directory as the script
+
 sound_file_path = os.path.join(script_dir, 'keysounds.mp3')  # Replace 'soundfile.mp3' with your actual sound file name
 sound = pygame.mixer.Sound(sound_file_path)
 
-# Function to play the sound asynchronously
+
 def play_sound():
     sound.play()
 
@@ -29,14 +29,14 @@ def on_key(event):
         print(f"Key pressed: {event.name}")
         threading.Thread(target=play_sound).start()
 
-# Create listeners for mouse events and key events
+
 mouse_listener = mouse.Listener(on_click=on_click)
 keyboard.hook(on_key)
 
-# Start the listeners in the background
+
 mouse_listener.start()
 
-# Wait for the listeners to finish (Ctrl+C to stop the script)
+
 try:
     mouse_listener.join()
 except KeyboardInterrupt:
